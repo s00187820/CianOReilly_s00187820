5878d0019d8cadf821a3ddeb6203dd71d121d461
CianO'Reilly <46957507+GitSmugg@users.noreply.github.com>
2020-05-13 11:26:58 +0100

    Q5 database framework

--- CianOReilly_s00187820/Phone.cs
+++ CianOReilly_s00187820/Phone.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CianOReilly_s00187820
 {
-    public class Phone
+    public class Phone 
     {
+        public Phone(string v1, int v2, string v3, string v4, string v5)
+        {
+        }
+
         //prop
         public string Name { get; set; }
         public decimal Price { get; set; }
@@ -15,11 +20,23 @@ namespace CianOReilly_s00187820
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
 
+        
+        
+        public List<Phone> Phones { get; set; }
+      
+
+
         //method
         public void IncreasePrice(decimal amount)
         {
             Price += amount;
         }
 
+    }
+
+    public class PhoneData : DbContext
+    {
+        public PhoneData() : base("MyPhoneData") { }
+        public DbSet<Phone> Phones { get; set; }
     }
 }
---
+++ DataMangement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CianOReilly_s00187820;
+
+namespace DataMangement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneData db = new PhoneData();
+
+            using (db)
+            {
+                Phone p1 = new Phone("Samsung S20", 500, "Android", "/images/android.png", "/images/s20.jpg");
+                Phone p2 = new Phone("IPhone 11", 600, "IOS", "/images/apple.png", "/images/iphone11.jpg");
+
+                db.Phones.Add(p1);
+                db.Phones.Add(p1);
+
+                Console.WriteLine("Added Phones to database");
+
+                db.SaveChanges();
+
+                Console.WriteLine("Saved to database");
+
+            }
+            
+
+        }
+    }
+}
