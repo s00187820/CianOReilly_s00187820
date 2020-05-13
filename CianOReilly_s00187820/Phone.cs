e2c36c1a8dc69c148a850c04cb7ee60b8d73b9b5
CianO'Reilly <46957507+GitSmugg@users.noreply.github.com>
2020-05-13 10:11:18 +0100

    Q2 create class phone with properties

---
+++ CianOReilly_s00187820/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CianOReilly_s00187820
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+    }
+}
